10326694122421f70c02ad3bed7a7fb64a5cb9de
Yousaf-Ansari-8666 <im8666305@gmail.com>
2025-04-20 22:42:18 +0500

    first commit

---
+++ src/components/contexts/TodoContext.js
@@ -0,0 +1,21 @@
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext({
+  todos: [
+    {
+      id: 1,
+      todo_msg: " learn js",
+      completed: false,
+    },
+  ],
+  addTodo: (todo) => {},
+  updatedTodo: (id, todo) => {},
+  deletedTodo: (id) => {},
+  toggleComplete: (id) => {},
+});
+
+export const TodoProvider = TodoContext.Provider
+
+export const useTodo = () => {
+  return useContext(TodoContext);
+};
